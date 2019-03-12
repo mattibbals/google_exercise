8ce5b5be510cc73e5341cef33a9d0c6201450558
Matthew Tibbals <matthewtibbals@Matthews-MBP-2.monkeybrains.net>
2019-03-12 05:32:04 -0700

    Updates

---
+++ precache-manifest.486f2d8f5836d13ca45da10750cb904c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5454d9c9b3d1ce5b63",
+    "url": "/google_exercise/static/css/main.7abad300.chunk.css"
+  },
+  {
+    "revision": "fa5454d9c9b3d1ce5b63",
+    "url": "/google_exercise/static/js/main.e5d44bc8.chunk.js"
+  },
+  {
+    "revision": "4816a8cbb0e54b766173",
+    "url": "/google_exercise/static/js/runtime~main.dbcbb6eb.js"
+  },
+  {
+    "revision": "135f2b16d689682c25c1",
+    "url": "/google_exercise/static/js/2.679e367e.chunk.js"
+  },
+  {
+    "revision": "7135cf57fdd845095289bde1453076ec",
+    "url": "/google_exercise/index.html"
+  }
+];
